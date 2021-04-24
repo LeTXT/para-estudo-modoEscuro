3fdcdf2b0934cf21328aa97545c59000f7934846
LeTXT <leandrogomesdeoliveira2@gmail.com>
2021-04-24 16:05:45 -0300

    Melhoria no conceito do botão

--- Modo/js/script.js
+++ Modo/js/script.js
@@ -1,33 +1,36 @@
 let escuro = document.querySelector('div#container')
-
+let input = document.querySelector('input')
 
 function modo() {
-    escuro.classList.add('white')
+    escuro.classList.add('black')
     
-    const input = document.createElement('input')
+    /*const input = document.createElement('input')
     
-    escuro.removeChild(container.children[0])
     escuro.appendChild(input)
+    escuro.removeChild(container.children[2]) 
+    
     
     
     input.setAttribute('type', 'button')
-    input.setAttribute('value', 'Aperte')
+    input.setAttribute('value', 'Aperte') */
     input.setAttribute('onclick', 'modo2()')
-
+    input.setAttribute('class', 'botao2')
     
 }
 
 function modo2() {
-    escuro.classList.remove('white')
+    escuro.classList.remove('black')
 
-   const input = document.createElement('input')
+   /*const input = document.createElements('input')
 
-    escuro.removeChild(container.children[0])
-    escuro.appendChild(input) 
+   escuro.appendChild(input)
+    escuro.removeChild(container.children[1]) 
+     
 
     input.setAttribute('type', 'button')
-    input.setAttribute('value', 'Aperte')
+    input.setAttribute('value', 'Aperte') */
     input.setAttribute('onclick', 'modo()')
+    input.setAttribute('class', 'botao')
 
     
 }
