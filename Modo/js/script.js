ae33ba33cf09c0b78c08cd75ae203b323890817a
LeTXT <leandrogomesdeoliveira2@gmail.com>
2021-04-24 18:14:46 -0300

    Implementação no modo

--- Modo/js/script.js
+++ Modo/js/script.js
@@ -1,18 +1,26 @@
 let escuro = document.querySelector('div#container')
 let input = document.querySelector('input')
+let card = document.querySelector('div.card')
 
 function modo() {
     escuro.classList.add('black')
+    card.classList.add('black')
     
     /*const input = document.createElement('input')
     
     escuro.appendChild(input)
     escuro.removeChild(container.children[2]) 
     
-    
-    
     input.setAttribute('type', 'button')
     input.setAttribute('value', 'Aperte') */
+
+    card.removeChild(card.children[1])
+    
+    const ht = document.createElement('h1')
+    ht.innerHTML = 'Modo Escuro'
+    
+    card.appendChild(ht)
+    
     input.setAttribute('onclick', 'modo2()')
     input.setAttribute('class', 'botao2')
     
@@ -20,6 +28,7 @@ function modo() {
 
 function modo2() {
     escuro.classList.remove('black')
+    card.classList.remove('black')
 
    /*const input = document.createElements('input')
 
@@ -29,6 +38,14 @@ function modo2() {
 
     input.setAttribute('type', 'button')
     input.setAttribute('value', 'Aperte') */
+
+    card.removeChild(card.children[1])
+    
+    const ht = document.createElement('h1')
+    ht.innerHTML = 'Modo Claro'
+    
+    card.appendChild(ht)
+
     input.setAttribute('onclick', 'modo()')
     input.setAttribute('class', 'botao')
 
